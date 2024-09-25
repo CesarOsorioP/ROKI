b16695f9cd11517998f655a6f39fea9a05a55a64
CesarOsorioP <osoriocesar024@gmail.com>
2024-09-24 22:31:23 -0500

    Se corrigen diversos bugs y se optimizan la interfaz usuario, registro y pagos

--- client/src/App.js
+++ client/src/App.js
@@ -5,26 +5,24 @@ import Signup from './Pages/Signup';
 import Login from './Pages/Login';
 import Reproductor from './Pages/Reproductor';
 import Pagospage from './Pages/Pagos';
-import Navbar from './Pages/Navbar';
-import PlayerControl from './Pages/PlayerControl';
 import UserInfo from './Components/Reproductor/UserInfo';
+
+
+
 
 function App() {
   return (
     <div className='main-content'>
       <BrowserRouter>
-        <Navbar />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/reproductor" element={<Reproductor />} />
-            <Route path="/pagos" element={<Pagospage />} />
-            <Route path="/user" element={<UserInfo />} /> {/* Nueva ruta para la información del usuario */}
-          </Routes>
-        </div>
-        <PlayerControl />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/reproductor" element={<Reproductor/>}/>
+          <Route path="/pagos" element={<Pagospage/>}/>
+          <Route path="/user" element={<UserInfo />} /> {/* Nueva ruta para la información del usuario */}
+        
+        </Routes>
       </BrowserRouter>
     </div>
   );
--- client/src/Components/Reproductor/Navbar.jsx
+++ client/src/Components/Reproductor/Navbar.jsx
@@ -9,14 +9,19 @@ function Navbar() {
     <nav className="navbar">
       <ul>
         <li><Link to="/user"><FaUser /> Usuario</Link></li> {/* Enlace actualizado */}
-        <li><Link to="/reproductor"><FaHome /> Inicio</Link></li>
+        <li><Link to="/"><FaHome /> Inicio</Link></li>
         <li><Link to="/explore"><FaSearch /> Explorar</Link></li>
         <h2>MI MÚSICA</h2>
         <li><Link to="/songs"><FaMusic /> Canción</Link></li>
         <li><Link to="/albums"><FaMusic /> Álbum</Link></li>
         <li><Link to="/artists"><FaMusic /> Artista</Link></li>
         <li><Link to="/playlists"><FaMusic /> Playlists</Link></li>
+        <li><Link to="/songs"><FaMusic /> Canción</Link></li>
+        <li><Link to="/albums"><FaMusic /> Álbum</Link></li>
+        <li><Link to="/artists"><FaMusic /> Artista</Link></li>
+        <li><Link to="/playlists"><FaMusic /> Playlists</Link></li>
         <h2>LISTAS DE REPRODUCCIÓN</h2>
+        <li><Link to="/create"><FaPlus /> Crear...</Link></li>
         <li><Link to="/create"><FaPlus /> Crear...</Link></li>
       </ul>
     </nav>
